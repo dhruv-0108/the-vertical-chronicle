7a007bb6845a919f77942c64164275ebd2d1f7b9
Dhruv <dhruvofficial0108@gmail.com>
2026-07-30 00:10:46 +0530

    Confine background image to hero section only

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,16 +22,15 @@ const Index = () => {
 
   return (
     <div className="relative min-h-screen bg-[#111] text-[#D1D1CB] font-sans selection:bg-white selection:text-black">
-      {/* Fixed Background Image Slideshow for entire website */}
-      <div className="fixed inset-0 z-0 bg-[#111]">
-        <img src="/images/1.jpg" alt="Dhruv Background 1" className={`absolute inset-0 w-full h-full object-cover object-top grayscale transition-opacity duration-1000 ease-in-out ${currentImage === 0 ? 'opacity-100' : 'opacity-0'}`} />
-        <img src="/images/2.jpg" alt="Dhruv Background 2" className={`absolute inset-0 w-full h-full object-cover object-top grayscale transition-opacity duration-1000 ease-in-out ${currentImage === 1 ? 'opacity-100' : 'opacity-0'}`} />
-        <div className="absolute inset-0 bg-black/60 mix-blend-multiply"></div>
-        <div className="absolute inset-0 bg-black/50"></div> {/* extra darkening for overall readability */}
-      </div>
-
       {/* Top Section */}
-      <div className="relative min-h-screen flex flex-col text-white selection:bg-white selection:text-black">
+      <div className="relative min-h-screen flex flex-col overflow-hidden text-white selection:bg-white selection:text-black">
+        {/* Background Image Slideshow for Top Section */}
+        <div className="absolute inset-0 z-0 bg-[#111]">
+          <img src="/images/1.jpg" alt="Dhruv Background 1" className={`absolute inset-0 w-full h-full object-cover object-top grayscale transition-opacity duration-1000 ease-in-out ${currentImage === 0 ? 'opacity-100' : 'opacity-0'}`} />
+          <img src="/images/2.jpg" alt="Dhruv Background 2" className={`absolute inset-0 w-full h-full object-cover object-top grayscale transition-opacity duration-1000 ease-in-out ${currentImage === 1 ? 'opacity-100' : 'opacity-0'}`} />
+          <div className="absolute inset-0 bg-black/60 mix-blend-multiply"></div>
+          <div className="absolute inset-0 bg-black/40"></div> {/* extra darkening for readability */}
+        </div>
 
         {/* Navbar and Hero Content wrapped in relative z-10 */}
         <div className="relative z-10 flex flex-col flex-1">
